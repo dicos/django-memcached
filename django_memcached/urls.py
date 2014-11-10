7e69b13e47eb389a6b0fafdad7c8c26b1ab8278b
Dmitry Kosolapov home self@dicos.ru <self@dicos.ru>
2014-11-10 20:34:38 +0600

    django >= 1.6 compatibility

--- django_memcached/urls.py
+++ django_memcached/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import *
+except ImportError:  # django >= 1.6
+    from django.conf.urls import patterns
+
 
 urlpatterns = patterns('',
     (r'^$', 'django_memcached.views.server_list'),
